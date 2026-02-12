1b800e8852323d54ffe9778f38214d1fe96a9854
Makendra <gamerchan756@gmail.com>
2026-02-12 14:05:29 -0500

    Try again

--- Group2Proposal1/ViewController.swift
+++ Group2Proposal1/ViewController.swift
@@ -9,9 +9,35 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var projectTitle: UILabel!
+    
+    
+    @IBOutlet weak var teamMembers: UIStackView!
+    
+    @IBOutlet weak var ideaDraft: UIButton!
+    
+    
+    @IBOutlet weak var timeRoles: UIButton!
+    
+    
+    @IBOutlet weak var planButton: UIButton!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+    }
+    
+    override func viewWillAppear(_ animated: Bool) {
+        projectTitle.layer.cornerRadius = 10.0
+        
+        teamMembers.layer.cornerRadius = 10.0
+        
+        ideaDraft.layer.cornerRadius = 10.0
+        
+        timeRoles.layer.cornerRadius = 10.0
+        
+        planButton.layer.cornerRadius = 10.0
+        
     }
 
 
